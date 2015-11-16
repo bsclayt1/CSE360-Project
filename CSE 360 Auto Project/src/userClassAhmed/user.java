514d656aecf7b763b2f1a25bf7d9a887b3408a18
OutofmyOrbit <robbieg123456789@gmail.com>
2015-11-15 20:18:55 -0700

    Moved User Class and JSON file to Project folder

--- CSE 360 Auto Project/src/userClassAhmed/user.java
+++ CSE 360 Auto Project/src/userClassAhmed/user.java
@@ -1,27 +1,62 @@
-package userClassAhmed;
+package helloworld;
 
-import RadioClassesAhmed.*;
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.io.File;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 
 public class user {
 	String currentName;
 	String names[];
-	int passwords[];//4 digits?
+	String passwords[];//4 digits?
 	station stations[][];
 	
 	
 	public user(){
-		passwords = null;
-		names = null;
-		currentName = "sample User Name";
+		
+		JSONParser parser = new JSONParser();
+		File f = new File("myFile.txt");
+  	  if(f.exists()){
+	      try {
+	    	  
+	    	  Object obj = parser.parse(new FileReader(f));
+	    	  JSONArray root = (JSONArray) obj;
+
+	  		passwords = new String[root.size()];
+	  		names = new String[root.size()];
+	  		
+	    	  for(int i = 0 ; i < root.size(); i++){
+	    		  names[i] = (String) ((JSONObject) root.get(i)).get("name");
+	    		  System.out.println((String) ((JSONObject) root.get(i)).get("name"));
+	    		  //System.out.println((String) ((JSONObject) root.get(i)).get("pswd"));
+	    		  passwords[i] =  (String) ((JSONObject) root.get(i)).get("pswd");
+	    		  
+	    	  }
+	        } catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	      }else{
+	    	  names = null;
+	    	  passwords = null;
+	      }
+		currentName = names[0];
 		stations = null;
 	}
 
 
-	public boolean login(String name, int pswd){
+	public boolean login(String name, String pswd){
 		if(names != null && passwords != null){
 			for(int i = 0 ; i < names.length ; i ++){
-				if(name == names[i]){
-					if(pswd == passwords[i]){
+				if(name.equals(names[i])){
+					if(pswd.equals(passwords[i])){
 						currentName = name;
 						
 						return true;
@@ -34,6 +69,37 @@ public class user {
 		return false;
 	}
 	
+	public void writeToFile(){
+		JSONObject obj = new JSONObject();
+
+	      obj.put("name", "ahmed");
+	      obj.put("pswd", "5566");
+	     JSONObject obj7 = new JSONObject();
+
+	      obj7.put("name", "coakley");
+	      obj7.put("pswd", "1234");
+	      
+	      JSONObject obj8 = new JSONObject();
+
+	      obj8.put("name", "ben");
+	      obj8.put("pswd", "1234");
+	      JSONObject obj9 = new JSONObject();
+
+	      obj9.put("name", "robert");
+	      obj9.put("pswd", "1234");
+
+	      JSONArray root = new JSONArray();
+	      root.add(obj);
+	      root.add(obj7);
+	      root.add(obj8);
+	      root.add(obj9);
+	      
+	      try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("myFile.txt"), "utf-8"))) {
+	    	    writer.write(root.toJSONString());
+	    	} catch (IOException ex) {
+	    	    // handle me
+	    	}  
+	}
 	public String getCurrentName() {
 		return currentName;
 	}
@@ -54,12 +120,12 @@ public class user {
 	}
 
 
-	public int[] getPasswords() {
+	public String[] getPasswords() {
 		return passwords;
 	}
 
 
-	public void setPasswords(int[] passwords) {
+	public void setPasswords(String[] passwords) {
 		this.passwords = passwords;
 	}
 
